a908591bf9a5eac151b327286733d4601b2206f8
atash <atash@LAPTOP-R2LDVLRR>
2024-03-01 21:49:52 -0600

    Add JSON serialization and deserialization functionality
    MyDataClass has been introduced to handle JSON data. Program.cs has been updated where a new person instance is created and serialized to JSON and then deserialized back. In addition, a data.json file has been added under the resources directory for illustrative purposes.

---
+++ MyDataClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsoleApp1
+{
+    public class MyDataClass
+    {
+        public class Person
+        {
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string EmailAddress { get; set; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 
-Console.WriteLine("Hello, World!");
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Person class
+            MyDataClass.Person person = new MyDataClass.Person
+            {
+                Name = "John Doe",
+                Age = 30,
+                EmailAddress = "john.doe@example.com"
+            };
+
+            // Serialize the Person object to JSON
+            string json = JsonSerializer.Serialize(person);
+            Console.WriteLine("Serialized JSON:");
+            Console.WriteLine(json);
+
+            // Deserialize the JSON back to a Person object
+            MyDataClass.Person deserializedPerson = JsonSerializer.Deserialize<MyDataClass.Person>(json);
+            Console.WriteLine("\nDeserialized Person object:");
+            Console.WriteLine($"Name: {deserializedPerson.Name}");
+            Console.WriteLine($"Age: {deserializedPerson.Age}");
+            Console.WriteLine($"EmailAddress: {deserializedPerson.EmailAddress}");
+        }
+    }
+}
