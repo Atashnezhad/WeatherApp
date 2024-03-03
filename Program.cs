0f07a12ccb03e799e6fe153c968aa82e3d4e1e3c
atash <atash@LAPTOP-R2LDVLRR>
2024-03-02 21:06:01 -0600

    Clean up code formatting and remove unnecessary spaces
    This commit refines the code layout by aligning long lines of codes for improved readability. Unnecessary spaces and lines, which previously interrupted the code flow, have also been removed.

--- Program.cs
+++ Program.cs
@@ -13,10 +13,7 @@ namespace ConsoleApp1
 
             // Construct the full path to the JSON file
             string jsonFilePath = "C:\\Users\\atash\\RiderProjects\\ConsoleApp1\\app_settings.json";
-            
             string parentDirectory = Directory.GetParent(Directory.GetParent(executableDirectory).FullName).FullName;
-
-
             try
             {
                 // Read the JSON file contents as a string
@@ -34,20 +31,20 @@ namespace ConsoleApp1
                 DateTime startDate = DateTime.Now.AddDays(-7);
                 DateTime endDate = DateTime.Now;
                 string fields = "temperature_2m,precipitation";
-
                 try
                 {
                     // Call GetWeatherData asynchronously and wait for the result
-                    string weatherData = await weatherApiClient.GetWeatherData(latitude, longitude, startDate, endDate, fields);
+                    string weatherData =
+                        await weatherApiClient.GetWeatherData(latitude, longitude, startDate, endDate, fields);
 
                     // Print the response to the console
                     Console.WriteLine(weatherData);
                     // parse the JSON string to a MyDataClass.WeatherObject object
-                    MyDataClass.WeatherObject weatherObject = JsonSerializer.Deserialize<MyDataClass.WeatherObject>(weatherData);
+                    MyDataClass.WeatherObject weatherObject =
+                        JsonSerializer.Deserialize<MyDataClass.WeatherObject>(weatherData);
                     // now save the parsed data in a new JSON file in the resources folder
                     string newJsonFilePath = Path.Combine(parentDirectory, "resources", "weather_data.json");
                     File.WriteAllText(newJsonFilePath, weatherData);
-                    
                 }
                 catch (Exception ex)
                 {
